3e8a852298dcee2161476884166f8b492fed1692
Nikita Ivanaev <nikita.ivanaev@payanyway.ru>
2024-11-15 18:58:31 +0300

    updated logo site

--- resources/views/modules/header.blade.php
+++ resources/views/modules/header.blade.php
@@ -3,6 +3,7 @@
 @endphp
 
 <header>
+    <a href="{{ route('products.index') }}"><img id="epmLogo"src="{{ asset('img/epm_logo.png') }}" alt="epm_logo"></a>
     <a href="{{ route('products.index') }}">Товары</a>
     <a href="{{ route('steam.index') }}">Пополнить steam</a>
     <a href="{{ route('account.index') }}">Аккаунт</a>
